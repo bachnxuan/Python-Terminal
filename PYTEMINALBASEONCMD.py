b8f89fd71b07865b1682d3238adb1122497b9c92
bachnxuan <76820483+bachnxuan@users.noreply.github.com>
2021-08-19 13:08:39 +0700

    Update and rename PYTEMINAL.py to PYTEMINALBASEONCMD.py

--- PYTEMINALBASEONCMD.py
+++ PYTEMINALBASEONCMD.py
@@ -3,18 +3,20 @@ import platform
 import sys
 
 pathtocd = ""
+print('PYTHON CMD [Based on cmd]')
 while True:
     if platform.system() == 'Windows':
         pass
     else:
-        print('THIS TERMINAL NOT USE FOR THIS OS')
+        print('SORRY, THIS TERMINAL NOT USE FOR THIS OS :(')
         break
     cmdscr = input(f"PYTHON TERMINAL | File: {pathtocd} |=> ")
     os.system(f'cmd /c "{cmdscr}"')
     if cmdscr == 'cd':
         try:
-            pathtocd = input('Enter full path: ')
+            pathtocd = input('Enter full path >>> ')
             os.chdir(pathtocd)
+            print(':) Complete')
         except FileNotFoundError:
             print('The file does not exist')
             pathtocd = ''
