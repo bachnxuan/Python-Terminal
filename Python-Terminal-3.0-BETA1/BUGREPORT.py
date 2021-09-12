3511282bcca656c962523947588901d6a82aac3c
Bachpooh <76820483+bachnxuan@users.noreply.github.com>
2021-09-12 11:29:56 +0700

    Add files via upload

---
+++ Python-Terminal-3.0-BETA1/BUGREPORT.py
@@ -0,0 +1,22 @@
+import smtplib, ssl
+from getpass import getpass
+
+port = 465  # For SSL
+BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
+
+	
+def bugreport():
+	print('\n {0}WARNING: Enable access to less secure apps on your email account.{2} \n {1}https://www.google.com/settings/security/lesssecureapps{2}'.format(RED, GREEN, END))
+	smtp_server = "smtp.gmail.com"
+	sender_email = input('Type your gmail: ')
+	receiver_email = "bachnxuan@gmail.com"
+	password = getpass("Type your password: ")
+
+	message = input('Your bug: ')
+
+	context = ssl.create_default_context()
+
+
+	with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+		server.login(sender_email, password)
+		server.sendmail(sender_email, receiver_email, message)
---
+++ Python-Terminal-3.0-BETA1/PYTERMINAL-ver-3.0-BETA1.py
@@ -0,0 +1,192 @@
+from getpass import getpass
+import pytube
+import sys
+from time import *
+import os
+import platform
+import getpass
+import psutil
+from datetime import datetime
+import smtplib, ssl
+import hashlib
+from FUNCTION import *
+from MINESWEEPER import *
+from FLAPPYBIRD import *
+from BUGREPORT import *
+
+BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
+
+print('CHECKING SYSTEM...')
+if platform.system() == 'Windows':
+    pass
+else:
+    print(f'Sorry, This Terminal Not Use For {platform.system()} :(')
+    exit()
+
+pathcache = os.getcwd()
+pathtocd = os.getcwd()
+pyterver = '3.0 BETA1'
+user = os.getlogin()
+
+print('LOADING TERMINAL...')
+
+print('''
+██████╗░██╗░░░██╗████████╗██╗░░██╗░█████╗░███╗░░██╗  ████████╗███████╗██████╗░███╗░░░███╗██╗███╗░░██╗░█████╗░██╗░░░░░
+██╔══██╗╚██╗░██╔╝╚══██╔══╝██║░░██║██╔══██╗████╗░██║  ╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║████╗░██║██╔══██╗██║░░░░░
+██████╔╝░╚████╔╝░░░░██║░░░███████║██║░░██║██╔██╗██║  ░░░██║░░░█████╗░░██████╔╝██╔████╔██║██║██╔██╗██║███████║██║░░░░░
+██╔═══╝░░░╚██╔╝░░░░░██║░░░██╔══██║██║░░██║██║╚████║  ░░░██║░░░██╔══╝░░██╔══██╗██║╚██╔╝██║██║██║╚████║██╔══██║██║░░░░░
+██║░░░░░░░░██║░░░░░░██║░░░██║░░██║╚█████╔╝██║░╚███║  ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║██║██║░╚███║██║░░██║███████╗
+╚═╝░░░░░░░░╚═╝░░░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝  ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚══════╝
+''')
+while True:
+    cmdscr = input(f"PT {pathtocd}>")
+    if cmdscr == 'exit':
+        break
+    elif cmdscr == 'help':
+        print('''PYTHON TERMINAL HELP :
+        exit : EXIT THE TERMINAL
+        cd : CHANGE DIRECTORY [YOU CAN USE PYTHON TERMINAL FILE SHORTCUT]
+        help : OPEN THE PYTHON TERMINAL HELP 
+        python : OPEN PYTHON SHELL AND PYTHON PROJECT (NOTE : PLEASE USE PYTHON ADD TO PATH)
+        crtuser : CREATE USER
+        color : CHANGE COLOR
+        sdcolor : SET TO DEFAULT COLOR
+        deluser : DELETE USER
+        cptinfo : YOUR COMPUTER INFO
+        ver : PYTHON TERMINAL VERSION
+        del :  DELETE FILE
+        copy : COPY FILE
+        copyfst : COPY FILE FAST [USING ROBUST COPY]
+        ptfschelp : PYTHON TERMINAL FILE SHORTCUT HELP
+        speedtst : SPEED TEST
+        downvHR : DOWNLOAD VIDEO ON YOUTUBE [HIGH RESOLUTION]
+        downvFR : DOWNLOAD VIDEO ON YOUTUBE [FIRST RESOLUTION]
+        clear : CLEAR TERMINAL SCREEN
+        wsl : WINDOWS SUBSYSTEM FOR LINUX [NOTE : LOGOUT TO EXIT WSL]
+        twifi : TURN WIFI ON OR OFF
+        plgames : PLAY GAMES
+        bugrp : BUG REPORT
+        ''')
+    elif cmdscr == 'ptfschelp':
+        print('''PYTHON TERMINAL FILE SHORTCUT HELP:
+        USERF = USER FOLDER
+        APPDF = APPDATA FOLDER
+        ALOCALF = LOCAL FOLDER
+        AROAMF = ROAMING FOLDER
+        ALLF = LOCALLOW FOLDER
+        SUF = STARTUP FOLDER
+        WDF = WINDOWS FOLDER
+        SYS32F = SYSTEM32 FOLDER
+        SYSW64F = SYSWOW64 FOLDER [FOR 64 BIT MACHINE]
+        ''')
+    elif cmdscr == 'python':
+        print('[1].OPEN THE PYTHON PROJECT    [2].OPEN PYTHON SHELL')
+        pyinp = input('[?]: ')
+        if pyinp == '1':
+            try:
+                pjpinp = input('ENTER FULL PROJECT PATH >>> ')
+                pjninp = input('ENTER PROJECT NAME >>> ')
+                os.system(f'cmd /c "py {pjpinp}\{pjninp}"')
+            except FileNotFoundError:
+                print('PROJECT NOT EXIST :(')
+        if pyinp == '2':
+            os.system(f'cmd /c "py"')
+    elif cmdscr == 'run':
+        run()
+    elif cmdscr == 'crtuser':
+        createuser()
+    elif cmdscr == 'deluser':
+        deleteuser()
+    elif cmdscr == 'color':
+        color = input('INPUT COLOR CODE FORM CMD: ')
+        os.system(f'cmd /c "color {color}"')
+    elif cmdscr == 'sdcolor':
+        os.system('cmd /c "color 07"')
+    elif cmdscr == 'cptinfo':
+        computerinfo()
+    elif cmdscr == 'ver':
+        print(f'''
+        PYTHON TERMINAL |VER {pyterver}|
+        ''')
+    elif cmdscr == 'del':
+        deletefile()
+    elif cmdscr == 'copy':
+        copyfile()
+    elif cmdscr == 'copyfst':
+        copyfilefast()
+    elif cmdscr == 'speedtst':
+        os.system('cmd /c "speedtest-cli"')
+    elif cmdscr == 'downvHR':
+        downloadyoutubevideo()
+    elif cmdscr == 'downvFR':
+        downloadyoutubevideoFR()
+    elif cmdscr == 'wsl':
+        os.system('cmd /c "wsl"')
+    elif cmdscr == 'twifi':
+        tonoff = input('Turn wifi (ON or OFF): ')
+        if tonoff == 'ON':
+            turnwifionoff(onoff='on')
+        if tonoff == 'OFF':
+            turnwifionoff(onoff='off')
+    elif cmdscr == 'plgames':
+        print('''LIST OF GAME:
+        [1].MINESWEEPER
+        [2].FLAPPY BIRD''')
+        askgame = input('[?]: ')
+        if askgame == '1':
+            gamemine()
+            print('NOTE : \'Shift + Click\' TO PUT THE FLAGS')
+        if askgame == '2':
+            gameflappy()
+    elif cmdscr == 'bugrp':
+        bugreport()
+    elif cmdscr == 'clear':
+        os.system('cmd /c "cls"')
+        print('''
+██████╗░██╗░░░██╗████████╗██╗░░██╗░█████╗░███╗░░██╗  ████████╗███████╗██████╗░███╗░░░███╗██╗███╗░░██╗░█████╗░██╗░░░░░
+██╔══██╗╚██╗░██╔╝╚══██╔══╝██║░░██║██╔══██╗████╗░██║  ╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║████╗░██║██╔══██╗██║░░░░░
+██████╔╝░╚████╔╝░░░░██║░░░███████║██║░░██║██╔██╗██║  ░░░██║░░░█████╗░░██████╔╝██╔████╔██║██║██╔██╗██║███████║██║░░░░░
+██╔═══╝░░░╚██╔╝░░░░░██║░░░██╔══██║██║░░██║██║╚████║  ░░░██║░░░██╔══╝░░██╔══██╗██║╚██╔╝██║██║██║╚████║██╔══██║██║░░░░░
+██║░░░░░░░░██║░░░░░░██║░░░██║░░██║╚█████╔╝██║░╚███║  ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║██║██║░╚███║██║░░██║███████╗
+╚═╝░░░░░░░░╚═╝░░░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝  ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚══════╝
+        ''')
+    elif cmdscr == 'cd':
+        try:
+            pathtocd = input('Enter path >>> ')
+            if pathtocd == 'USERF':
+                pathtocd = f'C:\\Users\\{user}'
+            if pathtocd == 'APPDF':
+                pathtocd = f'C:\\Users\\{user}\\AppData'
+            if pathtocd == 'ALOCALF':
+                pathtocd = f'C:\\Users\\{user}\\AppData\\Local'
+            if pathtocd == 'AROAMF':
+                pathtocd = f'C:\\Users\\{user}\\AppData\\Roaming'
+            if pathtocd == 'ALLF':
+                pathtocd = f'C:\\Users\\{user}\\AppData\\LocalLow'
+            if pathtocd == 'SUF':
+                pathtocd = f'C:\\Users\\{user}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'
+            if pathtocd == 'WDF':
+                pathtocd = 'C:\Windows'
+            if pathtocd == 'SYS32F':
+                pathtocd = 'C:\Windows\System32'
+            if pathtocd == 'SYSW64F':
+                print('CHECKING SYSTEM...')
+                if '32bit' in platform.architecture():
+                    print(f'IT\'S NOT FOR {platform.architecture()} :(')
+                    print('USE PATH CACHE...')
+                    pathtocd = pathcache
+                else:
+                    pathtocd = 'C:\Windows\SysWOW64'
+            os.chdir(pathtocd)
+            print(':) Complete')
+        except FileNotFoundError:
+            print('DIRECTORY NOT EXIST :(')
+            print('USE PATH CACHE...')
+            pathtocd = pathcache
+            os.chdir(pathtocd)
+    elif cmdscr == '':
+        print('PLEASE TYPE A COMMAND, TYPE \'help\' TO HELP')
+    else:
+        print('INCORRECT COMMAND, PLEASE TYPE A CORRECT COMMAND, TYPE \'help\' TO HELP')
+
+print('GOOOOOOOOOOOOD BYYYYYYYYYYE !!!!')
