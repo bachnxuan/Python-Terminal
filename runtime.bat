cb629254a50dfd8fee7d895dd5671d171c15d96c
Bachpooh <76820483+bachnxuan@users.noreply.github.com>
2022-04-23 10:58:35 +0700

    Update runtime.bat

--- runtime.bat
+++ runtime.bat
@@ -2,6 +2,5 @@
 python -m pip install --upgrade pip
 pip install psutil
 pip install speedtest-cli
-python -m smtpd -c DebuggingServer -n localhost:1025
 python -m pip install git+https://github.com/Zeecka/pytube@fix_1060
 python -m pip install --upgrade pytube
